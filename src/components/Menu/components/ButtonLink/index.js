6ae06ddd194ac4912bb527796a8d32c8f4068be3
zerorest <jrjoserodrigues42@gmail.com>
2020-12-29 20:09:08 +0000

    Initial commit

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ButtonLink(props) {
+    // props => { className: "o que alguem passar", href: "oq passar" } 
+
+    return ( <
+        a href = { props.href }
+        className = { props.className } > { props.children } <
+        /a>
+    );
+}
+
+export default ButtonLink;
